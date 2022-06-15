21de591d902b2c52040180b261b6506f7a7b58d2
Nguyen Kim Tung <72078935+tungnk181120@users.noreply.github.com>
2022-06-15 19:51:27 +0700

    deploy db

--- dist/database/migrations/1655036630393-user.d.ts
+++ dist/database/migrations/1655036630393-user.d.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 export declare class user1655036630393 implements MigrationInterface {
     up(queryRunner: QueryRunner): Promise<void>;
     down(queryRunner: QueryRunner): Promise<void>;
