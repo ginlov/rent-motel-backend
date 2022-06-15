12c887e316022516711003da4351c5aba471d028
tdd75 <tranducduy7520@gmail.com>
2022-06-15 20:42:50 +0700

    Revert "deploy db"
    This reverts commit 21de591d902b2c52040180b261b6506f7a7b58d2.

--- dist/database/migrations/1655036630393-user.js
+++ dist/database/migrations/1655036630393-user.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.user1655036630393 = void 0;
 class user1655036630393 {
-    async up(queryRunner) { }
-    async down(queryRunner) { }
+    async up(queryRunner) {
+    }
+    async down(queryRunner) {
+    }
 }
 exports.user1655036630393 = user1655036630393;
 //# sourceMappingURL=1655036630393-user.js.map
