962ddced9b584ef7baf1842d2a2da03c87b7faff
tdd75 <tranducduy7520@gmail.com>
2022-07-03 00:50:14 +0700

    feat: update get motel detail

--- src/motels/motels.module.ts
+++ src/motels/motels.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Motel } from './motel.entity';
 import { MotelsService } from './motels.service';
 import { MotelsController } from './motels.controller';
 import { AddressesModule } from '../addresses/addresses.module';
+import { RenterMotelModule } from '../renter-motel/renter-motel.module';
+import { MotelUtilityModule } from '../motel-utility/motel-utility.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Motel]), AddressesModule],
+  imports: [
+    TypeOrmModule.forFeature([Motel]),
+    AddressesModule,
+    forwardRef(() => RenterMotelModule),
+    forwardRef(() => MotelUtilityModule),
+  ],
   exports: [MotelsService],
   providers: [MotelsService],
   controllers: [MotelsController],
--- src/renter-motel/renter-motel.module.ts
+++ src/renter-motel/renter-motel.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { RenterMotelService } from './renter-motel.service';
 import { RenterMotelController } from './renter-motel.controller';
 import { UsersModule } from '../users/users.module';
@@ -7,7 +7,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RenterMotel } from './renter-motel.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RenterMotel]), MotelsModule],
+  imports: [
+    TypeOrmModule.forFeature([RenterMotel]),
+    forwardRef(() => MotelsModule),
+  ],
+  exports: [RenterMotelService],
   providers: [RenterMotelService],
   controllers: [RenterMotelController],
 })
